a0c0b38074f0ca3157e878574d574c677864f3cf
Angelo <97135832+apcvsnavarro@users.noreply.github.com>
2025-06-21 12:37:01 +0000

    added periods in the about

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,20 +3,20 @@
     <h2>About Me</h2>
     <p>Hi, I’m Angelo, an IT student passionate about technology and learning. This website is a space where I share a bit about myself, my interests, and my journey as a student. Whether you’re curious about my projects, want to connect, or just want to learn more about me, you’re in the right place.</p>
     <p>Feel free to explore, and don’t hesitate to reach out if you want to chat, collaborate, or just say hello. I’m always open to meeting fellow colleagues and exchanging ideas!</p>
-    <p>Thanks for visiting! </p>
+    <p>Thanks for visiting!</p>
     <h2>Hobbies and Interests</h2>
-    <p>I love playing a lot of video games on my free time. I am currently playing Stellar Blade</p>
-    <p>I also love playing music most of the time even when I'm playing video games or just cleaning my room</p>
-    <p>and a new hobby that my friend taught me about is building gunpla </p>
+    <p>I love playing a lot of video games on my free time. I am currently playing Stellar Blade.</p>
+    <p>I also love playing music most of the time even when I'm playing video games or just cleaning my room.</p>
+    <p>and a new hobby that my friend taught me about is building gunpla.</p>
     <h2>Education & Achievements</h2>
-    <p>I went to elementary and high school at St. Mary's Academy Pasay City</p>
-    <p>I graduated Senior High School at AMA Makati</p>
-    <p>I am currently studying Information Technology: Mobile/Internet at Asia Pacific College</p>
+    <p>I went to elementary and high school at St. Mary's Academy Pasay City.</p>
+    <p>I graduated Senior High School at AMA Makati.</p>
+    <p>I am currently studying Information Technology: Mobile/Internet at Asia Pacific College.</p>
     <h2>IT Experience</h2>
-    <p>I Assembled my first computer at age 13 by myself (Thanks Youtube)</p>
-    <p>I also helped a lot of friends build and troubleshoot their PCs throughout the years</p>
-    <p>I have completed work immersion at Development Bank of the Philippines (DBP) Head Office, working with the IT team on the bank's website</p>
-    <p>I have been working for lenovo as a tech guy for their events since last year</p>
+    <p>I Assembled my first computer at age 13 by myself (Thanks Youtube).</p>
+    <p>I also helped a lot of friends build and troubleshoot their PCs throughout the years.</p>
+    <p>I have completed work immersion at Development Bank of the Philippines (DBP) Head Office, working with the IT team on the bank's website.</p>
+    <p>I have been working for Lenovo as a tech guy for their events since last year.</p>
     <h2>Goals & Dreams</h2>
     <p>My goal is to be successful no matter what obstacles I encounter. I dream of visiting Japan or South Korea at least once in my life.</p>
   </section>
